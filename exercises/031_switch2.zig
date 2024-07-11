e6c115e9712a4ad00a787ae3d7bf351991c8e252
Nelis Santiago <143308422+nelisantiago@users.noreply.github.com>
2024-07-10 23:45:30 -0300

    Completed exercices 029 to 032

--- exercises/031_switch2.zig
+++ exercises/031_switch2.zig
@@ -16,19 +16,10 @@ pub fn main() void {
 
     for (lang_chars) |c| {
         const real_char: u8 = switch (c) {
-            1 => 'A',
-            2 => 'B',
-            3 => 'C',
-            4 => 'D',
-            5 => 'E',
-            6 => 'F',
-            7 => 'G',
-            8 => 'H',
-            9 => 'I',
-            10 => 'J',
-            // ...
-            25 => 'Y',
             26 => 'Z',
+            9 => 'I',
+            7 => 'G',
+            else => '!',
             // As in the last exercise, please add the 'else' clause
             // and this time, have it return an exclamation mark '!'.
         };
